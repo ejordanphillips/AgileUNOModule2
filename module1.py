a28acbcaaf8f50c7641507392866dd41ccbde2b9
Jordan Phillips <ejordanphillips@gmail.com>
2020-10-04 23:08:59 -0500

    Module 1 Code

---
+++ module1.py
@@ -0,0 +1,33 @@
+# Module 1
+# Jordan Phillips
+
+
+# Integer
+
+num1 = 1
+print(num1)
+
+# Float 
+
+num2 = 1.1
+print(num2)
+
+# Boolean
+
+status = True
+print(status)
+
+# String
+
+message = "Hello, world!"
+print(message)
+
+# Tuple
+
+tuple1 = (1, 2, 3, 4)
+print(tuple1)
+
+# Dictionary
+
+dict1 = {"key1": 1, "key2": 2, "key3": 3}
+print(dict1)
